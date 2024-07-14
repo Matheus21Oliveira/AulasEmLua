35cb563dce64e68a5638512406ce7c336a50cffd
Matheus21Oliveira <matheus21oliveira@gmail.com>
2024-07-14 12:02:25 -0300

    alteração de nome

--- aula01.lua
+++ aula01.lua
@@ -1,13 +1,1 @@
-playerlife = 100;
-playerattack = 10;
-playername = "Matheus"
-
-Enemylife = 100;
-enemyattack = 5;
-enemyname = "joao";
-
-luta1 = Enemylife - playerattack
-
-print("O",enemyname,"tem:",luta1,"de vida");
-
-
+print("hello world")
---
+++ aula02.lua
@@ -0,0 +1,16 @@
+playerlife = 100;
+playerattack = 10;
+playername = "Matheus"
+
+Enemylife = 100;
+enemyattack = 5;
+enemyname = "joao";
+
+luta1 = Enemylife - playerattack
+
+print("O",enemyname,"tem:",luta1,"de vida");
+
+print("foi uma bela luta");
+
+
+
