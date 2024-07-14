92572d9c7688afd8372a8309d6cd7c35a12c424a
Matheus21Oliveira <matheus21oliveira@gmail.com>
2024-07-14 11:47:16 -0300

    aulas 00 e 01

---
+++ aula01.lua
@@ -0,0 +1,13 @@
+playerlife = 100;
+playerattack = 10;
+playername = "Matheus"
+
+Enemylife = 100;
+enemyattack = 5;
+enemyname = "joao";
+
+luta1 = Enemylife - playerattack
+
+print("O",enemyname,"tem:",luta1,"de vida");
+
+
